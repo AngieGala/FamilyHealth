1f09303204eb12c5298dffb3f69735e8cba0b923
Angie Gala <anmigasa20@gmail.com>
2021-10-15 13:37:43 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/spring/FamilyHealthApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyHealthApplication.class, args);
+	}
+
+}
