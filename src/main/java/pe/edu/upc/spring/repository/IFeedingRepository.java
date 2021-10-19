61ba60f791b95221957655f90c35eb0b7d867f49
Angie Gala <anmigasa20@gmail.com>
2021-10-19 16:35:33 -0500

    Create IFeedingRepository.java

---
+++ src/main/java/pe/edu/upc/spring/repository/IFeedingRepository.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.spring.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import pe.edu.upc.spring.model.Feeding;
+
+@Repository
+public interface IFeedingRepository extends JpaRepository<Feeding, Integer>{
+	@Query("from Feeding f where f.patient.namePatient like %:namePatient%")
+	List<Feeding> buscarPaciente(@Param("namePatient") String namePatient);
+}
